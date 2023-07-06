02d2ce9ea1939f2a73769de3331fc93c36e61780
nickcopi <32046105+nickcopi@users.noreply.github.com>
2023-07-06 00:34:21 +0000

    Add self link vue component.

---
+++ src/.vuepress/components/SelfLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <a :href="getLinkUrl()" target="_blank">
+    {{ getLinkUrl() }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    port: {
+      type: Number,
+      required: true
+    },
+    protocol: {
+      type: String,
+      default: 'http://' // Default protocol value
+    },
+    path: {
+      type: String,
+      default: '/'
+    }
+  },
+  methods: {
+    getLinkUrl() {
+      const hostname = window.location.hostname;
+      return `${this.protocol}${hostname}:${this.port}${this.path}`;
+    }
+  }
+};
+</script>
